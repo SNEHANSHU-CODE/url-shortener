14fedb4fe30b8b2b34e5344cfec1eb83e096cbe8
Snehanshu <snehanshusekhar99@gmail.com>
2023-12-11 15:22:16 +0530

    Version-1.0.0

--- server/server.js
+++ server/server.js
@@ -85,7 +85,7 @@ if (process.env.NODE_ENV === 'production') {
   }
   
 
-const PORT = process.env .PORT || 5000;
+const PORT = 8080;
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
